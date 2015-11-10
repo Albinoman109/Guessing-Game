1bccbe33b6828d01fa0d54c734116d1ee7339f0a
Albinoman109 <albinoman109@gmail.com>
2015-11-09 16:53:58 -0800

    Add some new stuff!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Guessing-Game
 This is my crappy game!
+
+Joke!
+
+Knock, knock. Who’s there? Canoe. Canoe who? Canoe help me with my homework?
--- example.js
+++ example.js
@@ -1,10 +1,13 @@
+var num = Math.floor(Math.random() * 50);
+var easterEgg = 42;
 var myAge = 21;
-var dateOfBirth = 1994;
-var guessAge = prompt("Please guess my age!")
-var guessDoB= prompt("Please guess my date of birth!")
+var hitchHiker = "towel";
+var guessNum = prompt("Guess a number");
+var guessAge = prompt("Please guess my age!");
+var awesome = prompt("Is this game awesome? yes or no");
 
-if (guessAge === myAge) {
-	alert("Your right!");
+if (guessAge == myAge) {
+	alert("Your right! I am 21!");
 } else {
 	if (guessAge < myAge) {
 		alert("Your a little low!");
@@ -16,17 +19,38 @@ if (guessAge === myAge) {
 	}
 }
 
-if (guessDoB === dateOfBirth) {
-	alert("That right!");
-} else {
-	if (guessDoB < dateOfBirth) {
-		alert("Hey im not that young!");
+if (guessNum == num) {
+		alert("You win!!");
 	}
 	else {
-		if (guessDoB > dateOfBirth) {
-			alert("Thanks...but im not that old!");
+		if (guessNum == easterEgg) {
+			alert("Thats the answer to the Ultimate Question of Life, the Universe, and Everything!");
+		} else {
+			if (guessNum == hitchHiker) {
+				alert("You can't forget that!!");
+			} else {
+				if (guessNum < num) {
+					alert("Your number was to low!");
+				} else {
+					if (guessNum > num) {
+						alert("Your number was to high!!");
+					}
+				}
+			}
 		}
-	}
+	} 
+
+
+if (awesome === "yes" || awesome === "Yes") {
+    alert("Damn right it is!");
+
+} else if (awesome === "no" || awesome === "No") {
+    alert("Awwwwwwwww");
+
+} else {
+    alert("Waaay off..");
 }
+
 console.log("They thought I was" + " " + guessAge);
-console.log("They thought I was" + " " + guessDoB);
+console.log("Their guess was" + " " + guessNum);
+console.log("Do they think my game is awesome?" + " " + awesome);
